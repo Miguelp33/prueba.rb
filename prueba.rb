e94c120bddaf65c145ad306420bf9b2276ccd0f5
Miguel <salcedoperezmiguel.mps@gmail.com>
2018-04-03 19:25:06 -0400

    Enter your age

--- prueba.rb
+++ prueba.rb
@@ -5,4 +5,13 @@ if name == "MIGUEL"
     puts "Welcome boss #{name}"
 else 
     puts "Welcome idiot"
-end; puts
+end
+
+print "\nHow old are you : "
+age=gets.chomp.to_i ; puts
+
+if age >= 18
+    puts "You are a man "
+else 
+    puts "You are a baby "
+end ; puts
